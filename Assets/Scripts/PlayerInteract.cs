c84ecaaf71d49712abd5b83a6c045e67ef5c27ee
Brenden D <90849787+Brenden211@users.noreply.github.com>
2022-03-27 06:37:52 -0400

    Changes

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public bool flashlightActive = false;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Flashlight") && flashlightActive == false)
+        {
+            flashlightActive = true;
+        }
+
+        else if (Input.GetButtonDown("Flashlight") && flashlightActive == true)
+        {
+            flashlightActive = false;
+        }
+    }
+}
