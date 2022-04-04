90b2c6c32e27235fb459b08a21d087bccb5e125f
Brenden D <90849787+Brenden211@users.noreply.github.com>
2022-04-04 12:53:13 -0400

    Interact Fixes

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -7,22 +7,26 @@ public class Interactor : MonoBehaviour
 {
     UnityEvent onInteract;
 
+    public GameObject interactCanvas;
     public LayerMask interactableMask;
 
     void Update()
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 6, interactableMask))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, interactableMask))
         {
+
             if (hit.collider.GetComponent<Interactable>() != false)
             {
+                interactCanvas.SetActive(true);
                 onInteract = hit.collider.GetComponent<Interactable>().onInteract;
 
                 if (Input.GetKeyDown("e"))
                 {
                     Debug.Log("Interact!");
                     onInteract.Invoke();
+                    interactCanvas.SetActive(false);
                 }
             }
         }
