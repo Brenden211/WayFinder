b472f86229356fd413a630e4137b0e3495982073
Brenden D <90849787+Brenden211@users.noreply.github.com>
2022-04-06 01:52:59 -0400

    More

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -8,15 +8,28 @@ public class Interactor : MonoBehaviour
 {
     UnityEvent onInteract;
 
-    public GameObject interactCanvas;
+    public LayerMask flashlightMask;
     public LayerMask interactableMask;
-    public LayerMask keyMask;
+    public LayerMask Mask;
+    public LayerMask startingKeyMask;
     public LayerMask doorMask;
+    public LayerMask key1Mask;
+
+    public GameObject interactCanvas;
     public GameObject playerFlashLight;
     public GameObject startingKey;
     public GameObject flashlightModel;
     public GameObject startingKeyModel;
+    public GameObject noKeyMessage;
+    public GameObject key1Model;
+    public GameObject key1;
+
     public Animator doorAnimator;
+
+    public bool hasStartingKey;
+    public bool doorOpen;
+    public bool hasKey1;
+    public bool door1Open;
 
     private void Start()
     {
@@ -25,13 +38,21 @@ public class Interactor : MonoBehaviour
         startingKey.SetActive(false);
         flashlightModel.SetActive(true);
         startingKeyModel.SetActive(true);
+        noKeyMessage.SetActive(false);
+        key1.SetActive(false);
+        key1Model.SetActive(true);
+
+        hasStartingKey = false;
+        doorOpen = false;
+        hasKey1 = false;
+        door1Open = false;
     }
 
     void Update()
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 3, interactableMask))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 3, flashlightMask))
         {
 
             if (hit.collider.GetComponent<Interactable>() != false)
@@ -42,12 +63,13 @@ public class Interactor : MonoBehaviour
                 if (Input.GetKeyDown("e"))
                 {
                     playerFlashLight.SetActive(true);
+                    interactCanvas.SetActive(false);
                     flashlightModel.SetActive(false);
                     onInteract.Invoke();
                 }
             }
         }
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 3, keyMask))
+        else if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 3, startingKeyMask))
         {
             if (hit.collider.GetComponent<Interactable>() != false)
             {
@@ -57,28 +79,78 @@ public class Interactor : MonoBehaviour
                 if (Input.GetKeyDown("e"))
                 {
                     startingKey.SetActive(true);
+                    interactCanvas.SetActive(false);
                     startingKeyModel.SetActive(false);
+                    onInteract.Invoke();
+                    hasStartingKey = true;
+                }
+            }
+        }
+        else if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 5, doorMask))
+        {
+            if (hit.collider.GetComponent<Interactable>() != false && doorOpen == false && hasStartingKey == true || hasKey1 == true)
+            {
+                interactCanvas.SetActive(true);
+                onInteract = hit.collider.GetComponent<Interactable>().onInteract;
+
+                if (Input.GetKeyDown("e") && hasStartingKey == true)
+                {
+                    hasStartingKey = false;
+                    doorOpen = true;
+                    interactCanvas.SetActive(false);
+                    doorAnimator.SetBool("DoorOpen", true);
+                    startingKey.SetActive(false);
+                    onInteract.Invoke();
+                }
+                else if (Input.GetKeyDown("e") && hasStartingKey == false)
+                {
+                    noKeyMessage.SetActive(true);
+                    onInteract.Invoke();
+                }
+            }
+            else if (hit.collider.GetComponent<Interactable>() != false && door1Open == false && doorOpen == true)
+            {
+                interactCanvas.SetActive(true);
+                onInteract = hit.collider.GetComponent<Interactable>().onInteract;
+
+                if (Input.GetKeyDown("e") && hasKey1 == true)
+                {
+                    door1Open = true;
+                    interactCanvas.SetActive(false);
+                    doorAnimator.SetBool("Door1Open", true);
+                    key1.SetActive(false);
+                    onInteract.Invoke();
+                }
+                else if (Input.GetKeyDown("e") && hasKey1 == false)
+                {
+                    noKeyMessage.SetActive(true);
                     onInteract.Invoke();
                 }
             }
         }
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 5, doorMask))
+        else if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 5, key1Mask))
         {
             if (hit.collider.GetComponent<Interactable>() != false)
             {
+                hasKey1 = true;
                 interactCanvas.SetActive(true);
                 onInteract = hit.collider.GetComponent<Interactable>().onInteract;
 
                 if (Input.GetKeyDown("e"))
                 {
-                    doorAnimator.SetBool("DoorOpen", true);
+                    hasKey1 = false;
+                    key1.SetActive(true);
+                    interactCanvas.SetActive(false);
+                    key1Model.SetActive(false);
                     onInteract.Invoke();
+                    hasKey1 = true;
                 }
             }
         }
-        else if (!Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 3, interactableMask))
+        else if (!Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 5, interactableMask))
         {
             interactCanvas.SetActive(false);
+            noKeyMessage.SetActive(false);
         }
     }
 }
